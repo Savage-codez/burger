a167a6ec47670b10aca0fa760ebcc8832448849e
Herculano Ramos <herc@Herculanos-MacBook-Pro.local>
2018-12-04 19:39:59 -0500

    finished work

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Late Night Fats');
+INSERT INTO burgers (burgerName) VALUES ('Vegan Burger');
+INSERT INTO burgers (burgerName, devoured) VALUES ('Big Game Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Outlaw BBQ Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Meat Surprise', true);
+INSERT INTO burgers (burgerName) VALUES ('Ocean Dweller');
